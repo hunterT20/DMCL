40e18e927753a0e18e6a5be6948dda045c1d1782
Thanh Tuấn <thanhtuan200295@gmail.com>
2017-08-28 23:37:44 +0700

    main activity

--- app/src/main/java/com/hunterit/dmcl/view/activity/MainActivity.java
+++ app/src/main/java/com/hunterit/dmcl/view/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hunterit.dmcl;
+package com.hunterit.dmcl.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.hunterit.dmcl.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +19,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_chinhanh:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_giohang:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_goi:
                     mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_sanpham:
+                    mTextMessage.setText("Sản phẩm");
+                    return true;
+                case R.id.navigation_taikhoan:
+                    mTextMessage.setText("Tài khoản");
                     return true;
             }
             return false;
